8bde4353e60091170511931fb1f7a4004c6ea2aa
Josmar Dias <josmarnet@gmail.com>
2018-01-19 20:33:54 -0200

    chore: add code quality tools

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+/* global module */
+
+module.exports = {
+  parser: 'babel-eslint',
+  env: {
+    es6: true,
+    jest: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    './config/eslint/detect-errors.js',
+    './config/eslint/code-style.js',
+  ],
+  parserOptions: {
+    sourceType: 'module',
+  },
+}
